6018cc655b298aaf352f970798ca61ad79146038
Love jain <love.j@somaiya.edu>
2025-01-25 19:41:17 +0530

    intial layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  const Func = ()=> {
+
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/footer/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
