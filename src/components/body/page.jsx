493f242ee31b723eb32487152c3aea32c80c55f1
Love jain <love.j@somaiya.edu>
2025-01-26 20:09:25 +0530

    client and chatbot message UI

--- src/components/body/page.jsx
+++ src/components/body/page.jsx
@@ -1,11 +1,22 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import { User, Infinity } from 'lucide-react';
+import './index.css';
+
 const Body = () => {
   return (
     <>
-      <div className="chat-body-container">
-        <div className="message-container">
-          <p>Hello, how can I help you today?</p>
+      <div className='chat-body-container'>
+        <div className='chatbot-message-container'>
+        <div className='bot-logo'>
+            <Infinity size={33} color='grey' />
+          </div>
+          <p>hello, how can i help you?</p>
+        </div>
+        <div className='client-message-container'>
+          <p>There is an accident on the road , how will you handle it?</p>
+          <div className='client-logo'>
+            <User size={33} color='white' />
+          </div>
         </div>
       </div>
     </>
